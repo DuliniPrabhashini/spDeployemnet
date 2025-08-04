75e76984815194a9310df792ab68a4f6910cd2bc
DuliniPrabhashini <duliniprabhashini57@gmail.com>
2025-08-04 10:06:14 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse72/spdeployment/SpDeploymentApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse72.spdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpDeploymentApplication.class, args);
+    }
+
+}
